0ad5a6cc5719c3ba7ca4e11c0cfcd7075650fac7
Peter Lowe <peter.lowe@itcarlow.ie>
2017-12-05 13:55:14 +0000

    Add project files.

---
+++ MyVector3/main.cpp
@@ -0,0 +1,36 @@
+// <summary>
+/// small main program to test vector class
+/// included are some test to aid in writing correct methods
+/// and some for grading existing methods, you need to work on
+/// the former.
+/// </summary>
+#ifdef _DEBUG 
+#pragma comment(lib,"sfml-graphics-d.lib") 
+#pragma comment(lib,"sfml-audio-d.lib") 
+#pragma comment(lib,"sfml-system-d.lib") 
+#pragma comment(lib,"sfml-window-d.lib") 
+#pragma comment(lib,"sfml-network-d.lib") 
+#else 
+#pragma comment(lib,"sfml-graphics.lib") 
+#pragma comment(lib,"sfml-audio.lib") 
+#pragma comment(lib,"sfml-system.lib") 
+#pragma comment(lib,"sfml-window.lib") 
+#pragma comment(lib,"sfml-network.lib") 
+#endif 
+
+
+#include "MyVector3.h"
+#include <SFML\Graphics.hpp>
+#include <string.h>
+
+
+
+// you need more comments for Pete's sake
+
+int main()
+{
+	std::system("pause");
+	return EXIT_SUCCESS;
+}
+
+
